f28f3c6fa95da87e14b210f4cc2998b0a1726c2b
Jumal7 <justin.malinchak@gmail.com>
2017-03-21 15:17:52 -0400

    Here at initial batch files

--- run-sql-script.ps1
+++ run-sql-script.ps1
@@ -2,14 +2,14 @@
 # $s0=$args[0]
 $ScriptPath = Split-Path $MyInvocation.InvocationName
 
-#Mass loads data into the ipc-vsql01.DataAgg database
+#Mass loads data into the ISS database
 get-pssnapin -Registered
 Add-PSSnapin SqlServerCmdletSnapin100 # here lives Invoke-SqlCmd
 Add-PSSnapin SqlServerProviderSnapin100
-#invoke-sqlcmd -inputfile "$ScriptPath\mass-bulkinsert-to-ManagerConsensus.sql" -serverinstance "ipc-vsql01" -database "DataAgg" # the parameter -database can be omitted based on what your sql script does.
-#invoke-sqlcmd -inputfile "$ScriptPath\mass-bulkinsert-to-AUMOriginalCodeBlock.sql" -serverinstance "ipc-vsql01" -database "DataAgg" 
-#invoke-sqlcmd -inputfile "$ScriptPath\mass-bulkinsert-to-PricesOfSecuritiesFromGWP.sql" -serverinstance "ipc-vsql01" -database "DataAgg" 
-invoke-sqlcmd -inputfile "$ScriptPath\mass-bulkinsert-to-returnsyahoo.sql" -serverinstance "ipc-vsql01" -database "DataAgg" 
+
+invoke-sqlcmd -inputfile "$ScriptPath\mass-bulkinsert-to-WeightsOfIndexes.sql" -serverinstance "ipc-vsql01" -database "DataAgg" 
+
+#cmd.exe /c '\my-app\my-file.bat'
 
 #invoke-sqlcmd -inputfile "$ScriptPath\send_email_of_results_sql_managerconsensus_imported.sql" -serverinstance "ipc-vsql01" -database "DataAgg" 
 #invoke-sqlcmd -inputfile "$ScriptPath\send_email_of_results_sql_aumoriginalcodeblock_imported.sql" -serverinstance "ipc-vsql01" -database "DataAgg" 
